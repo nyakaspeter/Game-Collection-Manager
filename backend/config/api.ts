5d093a04bba8100683310ccde048586ca511d7ed
Nyakas Péter <thenyakaspeter@gmail.com>
2022-08-07 17:54:52 +0200

    enrich API response with IGDB data

--- backend/config/api.ts
+++ backend/config/api.ts
@@ -1,7 +1,11 @@
-export default {
+export default ({ env }) => ({
   rest: {
     defaultLimit: 25,
     maxLimit: 100,
     withCount: true,
   },
-};
+  igdb: {
+    twitchApiClientId: env("TWITCH_API_CLIENT_ID", ""),
+    twitchApiClientSecret: env("TWITCH_API_CLIENT_SECRET", ""),
+  },
+});
