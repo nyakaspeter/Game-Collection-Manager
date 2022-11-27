2252901308c285b7955aed8595ba6790ebd04eaa
Nyakas Péter <thenyakaspeter@gmail.com>
2022-11-27 10:39:30 +0100

    rename executable, new icon

--- src/utils/portable.ts
+++ src/utils/portable.ts
@@ -2,8 +2,8 @@ import { exists } from "@tauri-apps/api/fs";
 import { resolve, resourceDir } from "@tauri-apps/api/path";
 
 export const portableMode = await exists(
-  await resolve(await resourceDir(), "PORTABLE")
+  await resolve(await resourceDir(), "portable")
 );
 
 export const getStorePath = async (file: string) =>
-  portableMode ? await resolve(await resourceDir(), file) : file;
+  portableMode ? await resolve(await resourceDir(), ".gcm", file) : file;
